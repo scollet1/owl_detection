095b5ae871ce0a654434a9e540bac7660cda9c6c
Your Name <origamillamaa@gmail.com>
2017-10-21 20:17:33 +0000

    h

--- py/run.py
+++ py/run.py
@@ -1,20 +1,76 @@
 import numpy as np
 from keras.preprocessing import image
 from keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
-model = ResNet50(weights='imagenet')
+from preprocess import process
+
+# model = ResNet50(weights='imagenet')
 
 def train():
-	
+	batch_size = 16
 
-def predict():
-	model = objects.Network(3, 150, 150, 1)
+	# this is the augmentation configuration we will use for training
+	train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+	# this is the augmentation configuration we will use for testing:
+	# only rescaling
+	test_datagen = ImageDataGenerator(rescale=1./255)
+
+	# this is a generator that will read pictures found in
+	# subfolers of 'data/train', and indefinitely generate
+	# batches of augmented image data
+	train_generator = train_datagen.flow_from_directory(
+        'data/train',  # this is the target directory
+        target_size=(150, 150),  # all images will be resized to 150x150
+        batch_size=batch_size,
+        class_mode='binary')  # since we use binary_crossentropy loss, we need binary labels
+
+	# this is a similar generator, for validation data
+	validation_generator = test_datagen.flow_from_directory(
+        'data/validation',
+        target_size=(150, 150),
+        batch_size=batch_size,
+        class_mode='binary')
 	
+	model.fit_generator(
+        train_generator,
+        steps_per_epoch=2000 // batch_size,
+        epochs=50,
+        validation_data=validation_generator,
+        validation_steps=800 // batch_size)
+	model.save_weights('trained.h5')  # always save your weights after training or during training
+
+def predict(model, img):
+	batch_size = 16
+
+	predict_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+	predict_gen = predict_datagen.flow_from_directory(
+	'data/predict',  # this is the target directory
+        target_size=(150, 150),  # all images will be resized to 150x150
+        batch_size=batch_size,
+        class_mode='binary')  # since we use binary_crossentropy loss, we need binary labels
+	
+	model.predict_generator(
+	predict_gen,
+        steps_per_epoch=2000 // batch_size,
+        epochs=50,
+        validation_data=validation_generator,
+        validation_steps=800 // batch_size))
 
 def run(args):
+	model = objects.Network(3, 150, 150, 1)
 	if args[1] == '--train':
-		train()
+		train(model)
 	elif args[1] == '--predict':
-		predict([args[2]])
+		predict(model, [args[2]])
 
 if __name__== "__main__":
 	run(sys.argv)
